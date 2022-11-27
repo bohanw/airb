e97a6addb007dd609537b88e071d0d1a710cffe0
bohanw <bohanwang93@gmail.com>
2022-11-26 22:22:46 -0800

    comment in csv parser

--- src/main/java/csv_parser/CSVParser.java
+++ src/main/java/csv_parser/CSVParser.java
@@ -11,28 +11,55 @@ public class CSVParser {
         CSV Parser
         AirBnB Interview Question
         http://creativyst.com/Doc/Articles/CSV/CSV01.htm#EmbedBRs
+
+         https://hellosmallworld123.wordpress.com/2015/09/04/abnb-%E9%9D%A2%E7%BB%8F%E6%80%BB%E7%BB%93/
+        Input: csvformat
+    John,Smith,john.smith@gmail.com,Los Angeles,1
+    Jane,Roberts,janer@msn.com,"San Francisco, CA",0
+    "Alexandra ""Alex""",Menendez,alex.menendez@gmail.com,Miami,1 """Alexandra Alex"""
+
+    如果有逗号，转化成|
+如果有引号，把不考虑引号里逗号，把引号里的内容去引号整体打印。
+如果有两重引号，只去掉一重引号。
+
+    Output: escaped string
+    John|Smith|john.smith@gmail.com|Los Angeles|1
+    Jane|Roberts|janer@msn.com|San Francisco, CA|0
+    Alexandra "Alex"|Menendez|alex.menendez@gmail.com|Miami|1 "Alexandra Alex"
      */
     public class Solution {
         public String parseCSV(String str) {
+            //List to store stirng components delimited by ",",
+            //Will join with | in the outpu
             List<String> res = new ArrayList<>();
             boolean inQuote = false;
+            //Stringbuilder to build current CSV string component
             StringBuilder sb = new StringBuilder();
             for (int i = 0; i < str.length(); i++) {
                 if (inQuote) {
+                    //Deal with continuous quote( inside quotes)
                     if (str.charAt(i) == '\"') {
+                        //For \" if the next character is also quote, and inQuote==true,
+                        //Only need to remove the outermost quote
+                        //So need to append current quote
                         if (i < str.length() - 1 && str.charAt(i + 1) == '\"') {
                             sb.append("\"");
                             i++;
+                        //Reach the end of quote string
                         } else {
                             inQuote = false;
                         }
                     } else {
+                        //Keep building the current string component
                         sb.append(str.charAt(i));
                     }
                 } else {
+                    //Mark the start of a quoted string component
                     if (str.charAt(i) == '\"') {
                         inQuote = true;
                     } else if (str.charAt(i) == ',') {
+                        //Reach the end of  comma seperated component
+                        //add stringbuilder contents to list and reset the stringbuilder
                         res.add(sb.toString());
                         sb.setLength(0);
                     } else {
@@ -44,6 +71,7 @@ public class CSVParser {
             if (sb.length() > 0) {
                 res.add(sb.toString());
             }
+            //Return the result by joining res list strings wiht "|"
             return String.join("|", res);
         }
     }
